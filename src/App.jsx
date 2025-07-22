0fb2bb39226a36beff672e65e1a35729b0147fed
OxBryte <silascyrax@gmail.com>
2025-07-22 17:19:23 +0100

    feat: Add Roadmap page and update routing in App component to include new route

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/roadmap" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
